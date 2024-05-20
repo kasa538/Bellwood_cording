755c59f23cdb1f77ebc892edfb4b99e01bb06932
kasa538 <kyasya.990.hyarya@gmail.com>
2024-05-20 17:16:10 +0900

    feat: 検索結果詳細画面の詳細・ボタンを作成

--- js/custom.js
+++ js/custom.js
@@ -28,3 +28,43 @@
     }
   });
 })();
+
+// サムネイル
+const sliderThumbnail = new Swiper(".slider-thumbnail", {
+  slidesPerView: 3, // サムネイルの枚数
+});
+// スライダー
+const slider = new Swiper(".slider", {
+  loop: true,
+  // 前後の矢印
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: sliderThumbnail,
+  },
+});
+
+
+//（junpei）
+// // Swiperライブラリのスクリプトが正しくロードされた後に実行されるようにします。
+// document.addEventListener('DOMContentLoaded', function() {
+//   // サムネイル
+//   const sliderThumbnail = new Swiper(".slider-thumbnail", {
+//     slidesPerView: 3, // サムネイルの枚数
+//   });
+
+//   // スライダー
+//   const slider = new Swiper(".slider", {
+//     loop: true,
+//     // 前後の矢印
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     thumbs: {
+//       swiper: sliderThumbnail,
+//     },
+//   });
+// });
